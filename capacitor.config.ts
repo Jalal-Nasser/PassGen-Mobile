3712bf4ce3976a5c328f736d0c22da9e49342f8f
Jalal Nasser <25253619+Jalal-Nasser@users.noreply.github.com>
2026-02-24 22:41:19 +0300

    chore: updated ios app id to com.mdeploy.passgen to resolve conflict

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.passgen.app',
+  appId: 'com.mdeploy.passgen',
   appName: 'PassGen Vault',
   webDir: 'dist'
 };
