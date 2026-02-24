5c0304e7bb58047ff56dedad4cfb621d691f6c27
Jalal Nasser <25253619+Jalal-Nasser@users.noreply.github.com>
2026-02-24 21:19:29 +0300

    feat: add iOS platform to Capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.passgen.app',
+  appName: 'PassGen Vault',
+  webDir: 'dist'
+};
+
+export default config;
